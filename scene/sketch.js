74d9d8f6911ee46d9c58b707f6b3b828bfe43145
DaBearAhNL <deveranewell@gmail.com>
2026-09-24 13:04:31 -0600

    Update sketch.js

--- scene/sketch.js
+++ scene/sketch.js
@@ -5,13 +5,35 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x,y,dx,dy,accel;
+let dy = 1;
+let x = 50;
+let y = 50;
+let size = 100;
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+
+  gravitySim();
+  console.log(key);
+
+}
+
+
+function gravitySim() {
+  
   background(220);
-  rect(50,50,50,50);
+  square(x,y,size);
+
+  if (y + size > height) {
+    dy = 0;
+    y = height - size;
+    console.log(y+size);
+  }
+
+  dy = dy + 0.598;
+  y = y + dy;
+  console.log(dy);
 }
